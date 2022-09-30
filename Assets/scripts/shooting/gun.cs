3652adfe0c030d9deac7ee712af9bcf4d00a3634
Savelij Ognev <sava.ognev@gmail.com>
2022-09-30 16:57:46 +0200

    Changed shooting trails

--- Assets/scripts/shooting/gun.cs
+++ Assets/scripts/shooting/gun.cs
@@ -6,7 +6,6 @@ public class gun : MonoBehaviour
 {
     [SerializeField] private Transform firePoint;
     [SerializeField] private float fireRate;
-    [SerializeField] private float bulletForce;
     [SerializeField] private Camera shootCam;
     [SerializeField] private TrailRenderer bulletTrail;
     [SerializeField] private GameObject impact;
@@ -34,38 +33,41 @@ public class gun : MonoBehaviour
         }
         else
         {
-            targetPoint = ray.GetPoint(75);
+            targetPoint = ray.GetPoint(10);
         }
 
-        Debug.DrawLine(ray.origin, hit.point, Color.red);
+        Debug.DrawLine(ray.origin, targetPoint, Color.red);
 
         TrailRenderer trail = Instantiate(bulletTrail, firePoint.position, Quaternion.identity);
 
-        StartCoroutine(SpawnTrail(trail, hit));
+        StartCoroutine(SpawnTrail(trail, targetPoint));
 
-        if(hit.collider.tag == "Enemy")
+        if (hit.collider != null)
         {
-            hit.collider.GetComponent<AI>().health -= 10;
+            if (hit.collider.CompareTag("Enemy"))
+            {
+                hit.collider.GetComponent<AI>().health -= 10;
+            }
         }
 
         nextFire = Time.time + fireRate;
     }
 
-    private IEnumerator SpawnTrail(TrailRenderer trail, RaycastHit hit)
+    private IEnumerator SpawnTrail(TrailRenderer trail, Vector3 targetPoint)
     {
         float time = 0;
         Vector3 startPosition = firePoint.transform.position;
 
-        while(time <1)
+        while(time < 1)
         {
-            trail.transform.position = Vector3.Lerp(startPosition, hit.point, time);
+            trail.transform.position = Vector3.Lerp(startPosition, targetPoint, time);
             time += Time.deltaTime / trail.time;
 
             yield return null;
         }
 
-        trail.transform.position = hit.point;
-        Instantiate(impact, hit.point, Quaternion.LookRotation(hit.normal));
+        trail.transform.position = targetPoint;
+        Instantiate(impact, targetPoint, Quaternion.identity);
 
         Destroy(trail.gameObject, trail.time);
     }
