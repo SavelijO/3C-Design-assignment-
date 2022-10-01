21c78f5c5c59b1669d9c57f62c004747b4c7e052
sosokbomza <sava.ognev@gmail.com>
2022-10-01 19:39:23 +0200

    Dash + shotgun

--- Assets/scripts/controller/playerController.cs
+++ Assets/scripts/controller/playerController.cs
@@ -3,57 +3,87 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.XR;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class playerController : MonoBehaviour
 {
+    [Header("")]
+    [Header("Controls")]
     [SerializeField] private bool isUsingKeyboard;
     [SerializeField] public float deadzone;
-    [SerializeField] private float movSpeed;
-    [SerializeField] private float rotSpeed;
-
+    
     [Header("")]
     public Vector3 leftInput;
     public Vector3 rightInput;
 
+
     [Header("")]
+    [Header("Dash")]
+    [SerializeField] private float dashMultiplier;
+    [SerializeField] private float dashTime;
+    [SerializeField] private float dashCooldown;
+    private float currentDashTime = 0;
+    private float currentDashCooldown = 0;
+
+
+    [Header("")]
+    [Header("RigidBody")]
     [SerializeField] private Rigidbody playerRb;
+    [SerializeField] private float movSpeed;
+
+    [Header("")]
+    [Header("Model")]
     [SerializeField] private GameObject model;
     [SerializeField] public Transform firePoint;
+    [SerializeField] private float rotSpeed;
 
-
+    [Header("")]
+    [Header("Camera")]
     [SerializeField] private Camera rayCam;
     [SerializeField] private Transform rayCamPivot;
-    
 
+    private bool isMoving = false;
+    private bool isDashing = false;
+    private float time;
+
+
+
+    void Start()
+    {
+        if (!isUsingKeyboard) 
+        {
+            Cursor.visible = false;              
+        }
+    }
 
 
 
     void Update()
-    {     
-        if (isUsingKeyboard) {KeyboardUpdate();}
-        else { ControllerUpdate();}
+    {
+        time = Time.time;
+        if (isUsingKeyboard) { GetKeyboardInput(); }
+        else { GetControllerInput();}
+        
+        
+        CheckDash();
+        RotateModel();
+        MoveModel();
+               
     }
 
 
     void FixedUpdate()
     {
-        playerRb.MovePosition(transform.position + (transform.forward * leftInput.magnitude) * movSpeed * Time.fixedDeltaTime);
-        model.transform.position = playerRb.position;
+        if (isDashing) { Dash(); }
+        else
+        {
+            RotateRigidBody();
+            UpdateRigidBodyMovement();
+        }
     }
 
-    void KeyboardUpdate()
-    {
-        GetKeyboardInput();
-        RotateRigidBody();
-        RotateModel();
-    }
 
-    void ControllerUpdate()
-    {
-        GetControllerInput();
-        RotateRigidBody();
-        RotateModel();
-    }
+
 
     void GetKeyboardInput()
     {
@@ -76,26 +106,59 @@ public class playerController : MonoBehaviour
     void GetControllerInput()
     {
         Vector2 stickInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        if (stickInput.magnitude < deadzone) { stickInput = Vector2.zero; }
+        if (stickInput.magnitude < deadzone) { stickInput = Vector2.zero; isMoving = false; }
+        else { isMoving = true; }
         leftInput = new Vector3(stickInput.x, 0, stickInput.y);
-
 
         stickInput = new Vector2(Input.GetAxisRaw("HorizontalRight"), Input.GetAxisRaw("VerticalRight"));
         if (stickInput.magnitude < deadzone) { stickInput = Vector2.zero; }
         rightInput = new Vector3(stickInput.x, 0, stickInput.y);
         rightInput = rightInput.ToIso();
-
     }
 
     void RotateRigidBody()
     {
-        if (leftInput != Vector3.zero)
+        if (leftInput != Vector3.zero && !isDashing)
         {
             Quaternion isoRot = Quaternion.LookRotation(leftInput.ToIso(), Vector3.up);
 
             playerRb.transform.rotation = isoRot;
         }
     }
+
+    void UpdateRigidBodyMovement()
+    {
+        playerRb.MovePosition(transform.forward * leftInput.magnitude * movSpeed * Time.fixedDeltaTime + playerRb.position);
+    }
+
+    
+    void CheckDash()
+    {
+        if (currentDashCooldown < time && isMoving && Input.GetButtonDown("Dash"))
+        {
+            isDashing = true;
+            currentDashTime = dashTime + time;
+            currentDashCooldown = dashCooldown + time;
+        }
+        else if (currentDashTime > time)
+        {
+            currentDashTime -= Time.deltaTime;
+        }
+        else if (currentDashCooldown > time)
+        {
+            isDashing = false;
+            currentDashCooldown -= Time.deltaTime;
+        }
+    }
+
+    void Dash()
+    {
+
+        playerRb.MovePosition(transform.position + transform.forward * movSpeed * dashMultiplier * Time.fixedDeltaTime);
+    }
+    
+
+ 
 
     void RotateModel()
     {
@@ -105,16 +168,22 @@ public class playerController : MonoBehaviour
 
             Quaternion isoRot = Quaternion.LookRotation(leftInput.ToIso(), Vector3.up);
 
-            model.transform.rotation = Quaternion.RotateTowards(model.transform.rotation, isoRot, rotSpeed * 360 *Time.deltaTime);
+            model.transform.rotation = Quaternion.RotateTowards(model.transform.rotation, isoRot, rotSpeed * 360 * Time.fixedDeltaTime);
         }
-        if(rightInput != Vector3.zero)
+        if(rightInput != Vector3.zero && !isDashing)
         {
             Quaternion isoRot = Quaternion.LookRotation(rightInput, Vector3.up);
 
 
-            model.transform.rotation = Quaternion.RotateTowards(model.transform.rotation, isoRot, rotSpeed *  360 * Time.deltaTime);
+            model.transform.rotation = Quaternion.RotateTowards(model.transform.rotation, isoRot, rotSpeed *  360 * Time.fixedDeltaTime);
         }
 
     }
+
+    void MoveModel()
+    {
+        model.transform.position = playerRb.position;
+    }
+
 
 }
--- Assets/scripts/shooting/gun.cs
+++ Assets/scripts/shooting/gun.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class gun : MonoBehaviour
 {
-    [SerializeField] private Transform firePoint;
+    [Header("Gun Properties")]
     [SerializeField] private float fireRate;
-    [SerializeField] private Camera shootCam;
+    [SerializeField] private int arc;
+    [SerializeField] private int maxShootingDistance;
+    [SerializeField] private int bulletCount;
+    [SerializeField] private float intialSpread;
+    [SerializeField] private float postShotSpread;
+    [Header("")]
     [SerializeField] private TrailRenderer bulletTrail;
     [SerializeField] private GameObject impact;
+    [SerializeField] private Transform firePoint;
+
 
 
 
     private float nextFire = 0f;
-    void FixedUpdate()
+    void Update()
     {
-        if(Input.GetAxisRaw("Fire1") != 0 && Time.time > nextFire)
+        if (Input.GetAxisRaw("Fire1") != 0 && Time.time > nextFire)
         {
             Fire();
         }
@@ -23,40 +30,53 @@ public class gun : MonoBehaviour
 
     void Fire()
     {
-        Ray ray = shootCam.ViewportPointToRay(new Vector3(0.5f,0.5f,0));
-        RaycastHit hit;
+        Vector3 correctedForward = (firePoint.forward * Mathf.Cos(-arc / 2 * Mathf.Deg2Rad) + firePoint.right * Mathf.Sin(-arc / 2 * Mathf.Deg2Rad)).normalized;
+        Vector3 correctedRight = Quaternion.AngleAxis(90, Vector3.up) * correctedForward;
+        
+        float rayStep = arc / (bulletCount - 1);
+        for (int i = 0; i < bulletCount; i++)
+		{
+            float rayAngle = rayStep * i * Mathf.Deg2Rad;
+            Vector3 rayDir = (correctedForward * Mathf.Cos(rayAngle) + correctedRight * Mathf.Sin(rayAngle)).normalized;
+            
+            float xSpread = Random.Range(-postShotSpread, postShotSpread);
+            float ySpread = Random.Range(-postShotSpread, postShotSpread);
+            
+            Vector3 correctedDir = rayDir + new Vector3(xSpread, ySpread, 0);
+            
+            Ray ray = new Ray(firePoint.position + new Vector3(Random.Range(-intialSpread, intialSpread),0, Random.Range(-intialSpread, intialSpread)), correctedDir);
+            RaycastHit hit;
 
-        Vector3 targetPoint;
-        if(Physics.Raycast(ray, out hit))
-        {
-            targetPoint = hit.point;
-        }
-        else
-        {
-            targetPoint = ray.GetPoint(10);
-        }
-
-        Debug.DrawLine(ray.origin, targetPoint, Color.red);
-
-        TrailRenderer trail = Instantiate(bulletTrail, firePoint.position, Quaternion.identity);
-
-        StartCoroutine(SpawnTrail(trail, targetPoint));
-
-        if (hit.collider != null)
-        {
-            if (hit.collider.CompareTag("Enemy"))
+            Vector3 targetPoint;
+            if (Physics.Raycast(ray, out hit, maxShootingDistance))
             {
-                hit.collider.GetComponent<AI>().health -= 10;
+                targetPoint = hit.point;
+            }
+            else
+            {
+                targetPoint = ray.GetPoint(maxShootingDistance);
+            }
+
+            TrailRenderer trail = Instantiate(bulletTrail, firePoint.position, Quaternion.identity);
+
+            StartCoroutine(SpawnTrail(ray, hit, trail, targetPoint));
+
+            if (hit.collider != null)
+            {
+                if (hit.collider.CompareTag("Enemy"))
+                {
+                    hit.collider.GetComponent<AI>().health -= 10;
+                }
             }
         }
-
+        
         nextFire = Time.time + fireRate;
     }
 
-    private IEnumerator SpawnTrail(TrailRenderer trail, Vector3 targetPoint)
+    private IEnumerator SpawnTrail(Ray ray, RaycastHit hit, TrailRenderer trail, Vector3 targetPoint)
     {
         float time = 0;
-        Vector3 startPosition = firePoint.transform.position;
+        Vector3 startPosition = ray.origin;
 
         while(time < 1)
         {
@@ -67,7 +87,8 @@ public class gun : MonoBehaviour
         }
 
         trail.transform.position = targetPoint;
-        Instantiate(impact, targetPoint, Quaternion.identity);
+        if (hit.normal != Vector3.zero) { Instantiate(impact, targetPoint, Quaternion.LookRotation(hit.normal)); }
+        else { Instantiate(impact, targetPoint, Quaternion.identity); }
 
         Destroy(trail.gameObject, trail.time);
     }
